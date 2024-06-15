ca12e10eeadd7db8079da3cce2f0befb3cee1f31
starovit <ivanstarovit@gmail.com>
2024-06-15 22:52:41 +0300

    dats reader

--- src/src/src/utils.py
+++ src/src/src/utils.py
@@ -1,6 +1,5 @@
-import numpy as np
 import pandas as pd
-
+import numpy as np
 from numpy.linalg import norm
 
 def read_pickles(path_to_folder):
@@ -9,8 +8,20 @@ def read_pickles(path_to_folder):
     df_ratings = pd.read_pickle(path_to_folder+"ratings.pickle")
     return df_movies, df_users, df_ratings
 
-def read_dats(path_to_folder):
-    pass
+def read_dats(path_to_folder="../../data/ml-1m/"):
+    # read ratings
+    filename = "ratings.dat"
+    df_ratings = pd.read_csv(path_to_folder+filename, header=None, sep="::")
+    df_ratings.columns = ["UserID", "MovieID", "Rating", "Timestamp"]
+    # read users
+    filename = "users.dat"
+    df_users = pd.read_csv(path_to_folder+filename, header=None, sep="::")
+    df_users.columns = ["UserID", "Gender", "Age", "Occupation", "Zip-Code"]
+    # read movies
+    filename = "movies.dat"
+    df_movies = pd.read_csv(path_to_folder+filename, header=None, sep="::")
+    df_movies.columns = ["MovieID", "Title", "Genres"]
+    return df_movies, df_users, df_ratings
 
 class TrainTestSplitter():
     def __init__():
